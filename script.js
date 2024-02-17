c8c9ee0177a1871461d5646a266f3a9f40c69254
thisisAdrian142 <EmailAdrian.png@gmail.com>
2024-02-17 11:51:34 +1100

    17 Feb: Basic parllax effect page with nav bar

--- script.js
+++ script.js
@@ -8,9 +8,30 @@ function factorialize(num) {
     }
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    var line2 = prompt("Please type the number you would like to know its factorial in here: ");
+    line2 = parseInt(line2);
+    const outputArea = document.getElementById("factorial-result");
+    
+    outputArea.classList.remove("success-result", "error-result");
 
-var line2 = prompt("Please type the number you would like to know its factorial in here: ");    
-const outputArea = document.getElementById("factorial-result");
-const calculatedResult = factorialize(line2);
-outputArea.textContent = "The output result of this factorial is: " + calculatedResult;
+    if (!isNaN(line2)){
+        const calculatedResult = factorialize(line2);
+        outputArea.textContent = "The Javascript output result of this factorial is: " + calculatedResult;
+        outputArea.classList.add("success-result");
+    } else {
+        outputArea.textContent = "Please enter a valid number.";
+        outputArea.classList.add("error-result");
+    }
+})
 
+let sun = document.getElementById('sun-lake');
+let mountains = document.getElementById('mountains-lake');
+let cloud = document.getElementById('cloud-lake');
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+    sun.style.top = value * 2.0 + 'px';
+    mountains.style.top = value *2.4 + 'px';
+    cloud.style.right = value *-2.5 + 'px';
+})
